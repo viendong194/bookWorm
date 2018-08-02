9aefdd77613f32a180746e880f5ff617612e48b0
VAN HUYNH VIEN DONG <dongvhv@caters.co.jp>
2018-08-02 11:06:50 +0900

    fix image page

--- client/src/components/pages/ImagePage.js
+++ client/src/components/pages/ImagePage.js
@@ -3,25 +3,13 @@ import { connect } from 'react-redux';
 import EditImageForm from '../form/EditImageForm';
 import NewImageForm from '../form/NewImageForm';
 class ImagePage extends Component {
-	state = {
-		image: null
-	};
-	componentDidMount() {
-		if (this.props.match.params.id) {
-      let id = this.props.match.params.id;
-      setTimeout(()=>console.log(this.props.images),200);
-			let temp = null;
-			temp = this.props.images.filter((el) => el._id === id)[0];
-      console.log(this.props.images);
-      console.log(temp);
-			this.setState({ image: temp },()=>console.log(this.state.image));
-		}
-	}
 	render() {
+    let id = this.props.match.params.id;
+    const temp = this.props.images.filter((el) => el._id === id)[0];
 		return (
 			<div>
 				{this.props.match.params.id ? (
-					<EditImageForm image={this.state.image} />
+					<EditImageForm image={temp} />
 				) : (
 					<NewImageForm />
 				)}
